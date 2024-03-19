7dbc53247f5811cc07733901245252553d8a1ffe
staceyjf <staceyfanner@gmail.com>
2024-03-19 21:42:56 +1100

    working date

--- js/DOM/clock.js
+++ js/DOM/clock.js
@@ -1,6 +1,16 @@
 export function updateClockEl(calculateTimeDate) {
+  // refreshes every 1 sec to give the latest time
   setInterval(() => {
     const timeString = calculateTimeDate();
-    console.log("time in clock.js", timeString);
+    // modify 19/03/2024 to 19.03.24
+    const formattedDateStr = (
+      timeString.slice(0, 5) +
+      "." +
+      timeString.slice(8, 10)
+    ).replace(/\//g, ".");
+    const formattedTimeStr = null;
+
+    // console.log("time in clock.js: ", timeString);
+    console.log("formattedDateStr in clock.js: ", formattedDateStr);
   }, 1000);
 }
