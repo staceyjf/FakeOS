a4e3fcfffad5b1a25e41e8948267fe1db0fb10df
staceyjf <staceyfanner@gmail.com>
2024-03-21 15:14:18 +1100

    rework openModal to take a swtich instead of if /else

--- js/DOM/openModal.js
+++ js/DOM/openModal.js
@@ -2,11 +2,16 @@ export default function openModal(allIcons) {
   // add an event listener to all relevant icons
   allIcons.forEach((icon) => {
     icon.addEventListener("click", () => {
-      if (icon.id === "printerBtn") {
-        console.log("printer is working");
-        window.print(); // opens the
-      } else {
-        console.log("open a modal");
+      switch (icon.id) {
+        case icon.id === "printerBtn":
+          console.log("printer is working");
+          window.print(); // opens the built print function
+          break;
+        // case id === "printerBtn":
+        // window.print(); // opens the built print function
+        // break;
+        default:
+          alert("This icon is just for show");
       }
     });
   });
