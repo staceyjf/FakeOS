19e7a8edde9fa8c7852d96ece3092364dbc46a3c
staceyjf <staceyfanner@gmail.com>
2024-03-21 23:47:07 +1100

    revision to id names for close btns

--- js/DOM/modalHandler.js
+++ js/DOM/modalHandler.js
@@ -4,16 +4,15 @@ const modalCloseBtns = document.querySelectorAll(".modal--close");
 console.log("this are my close btns", modalCloseBtns);
 
 export function openModal({ icons, jokeParagraph, category }) {
-  // add an event listener to all relevant icons
   icons.forEach((icon) => {
     icon.addEventListener("click", () => {
       switch (icon.id) {
         case "printerBtn":
           console.log("printer is working");
-          window.print(); // opens the built print function
+          window.print(); // opens the window's print method
           break;
-        case "chuckOpenBtn":
-          displayModal(icon);
+        case "chuckBtn":
+          displayModal();
           console.log("Chuck has been opened", icon);
           addJokes(jokeParagraph, category);
           break;
@@ -28,17 +27,17 @@ export function openModal({ icons, jokeParagraph, category }) {
 
 export function closeModal({ modals }) {
   // add an event listener to all modals
-  modalCloseBtns.forEach((icon) => {
-    icon.addEventListener("click", () => {
-      switch (icon.id) {
+  modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+      switch (modalCloseBtn.id) {
         case "printerBtn":
           console.log(
             "printer doesn't need a case but will keep for refactoring into one function"
           );
           break;
         case "chuckCloseBtn":
-          hidModal(icon);
-          console.log("Chuck has closed", icon);
+          hidModal(modalCloseBtn);
+          console.log("Chuck has closed", modalCloseBtn);
           break;
         default:
           throw new Error("There has been an error with closing the modal");
@@ -55,4 +54,8 @@ function displayModal(modalEl) {
 // close
 function hidModal(modalEl) {
   modalEl.classList.remove("show");
+}
+
+function findModal(btnId) {
+  return document.getElementById(id);
 }
