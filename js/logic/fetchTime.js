a644130d734d17ca8feb4aef918459da5f09e67f
staceyjf <staceyfanner@gmail.com>
2024-03-26 15:55:22 +1100

    additional console.logs

--- js/logic/fetchTime.js
+++ js/logic/fetchTime.js
@@ -2,5 +2,6 @@ export default function calculateTimeDate() {
   let currentTime = new Date();
   // get local time and date for the user
   let currentLocalTime = currentTime.toLocaleString();
+  console.log("this is the current time " + currentLocalTime);
   return currentLocalTime;
 }
